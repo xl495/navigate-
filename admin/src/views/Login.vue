9a1e5d9039622b6572df84193db8591ba90f8964
xl495 <453007797@qq.com>
2020-01-05 17:17:46 +0800

    初始化

--- admin/src/views/Login.vue
+++ admin/src/views/Login.vue
@@ -1,48 +1,17 @@
 <template>
-  <div class="login-wrap">
-    <el-card class="box-card">
-      <div slot="header" class="clearfix">
-        <span>登录</span>
-      </div>
-      <el-form :model="model" label-width="80px">
-        <el-form-item label="名称">
-          <el-input v-model="model.name"></el-input>
-        </el-form-item>
-        <el-form-item label="密码">
-          <el-input v-model="model.password" type="password"></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="onSubmit">立即创建</el-button>
-        </el-form-item>
-      </el-form>
-    </el-card>
+  <div class="login">
+   
   </div>
 </template>
-<script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
 
-@Component({
-  name: "Login",
-})
-export default class extends Vue {
-  model = {};
+<script>
 
-  async onSubmit() {
-    this.$http.post({...this.model}).then(res=>{
-      console.log(res)
-    })
-    console.log(this.model)
+export default {
+  name: "login",
+  data() {
+    return {
+      
+    }
   }
-}
+};
 </script>
-<style lang="scss" scoped>
-.login-wrap {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  .box-card {
-    width: 480px;
-  }
-}
-</style>
